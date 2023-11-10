654a8276e0d846812911bac44b206e2a4b9ae407
이태경 <korea9300@naver.com>
2023-11-10 14:14:26 +0900

    Addddddd

---
+++ frontend/src/components/common/TheHeadingNavbar.vue
@@ -0,0 +1,29 @@
+<script setup></script>
+
+<template>
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+    <ul class="navbar-nav me-auto">
+      <li class="nav-item"><a class="nav-link" href=""> map </a></li>
+      <li class="nav-item"><a class="nav-link" href=""> user </a></li>
+      <li class="nav-item"><a class="nav-link" href=""> board </a></li>
+    </ul>
+    <!-- <c:choose> -->
+    <!-- session에 값이 없다면 로그인이 안됐다 !  -->
+    <!-- <c:when test="${empty userInfo}"> -->
+    <ul class="navbar-nav">
+      <li class="nav-item"><a class="nav-link" href="${root}/member/login">로그인</a></li>
+      <li class="nav-item"><a class="nav-link" href="${root}/member/regist">회원가입</a></li>
+    </ul>
+    <!-- </c:when> -->
+    <!-- session에 값이 있다면 로그인 됬다 !  -->
+    <!-- <c:otherwise> -->
+    <ul class="navbar-nav">
+      <li class="nav-item"><a class="nav-link disabled">${userInfo}님 반갑습니다.</a></li>
+      <li class="nav-item"><a class="nav-link" href="${root}/member/logout">로그아웃</a></li>
+    </ul>
+    <!-- </c:otherwise> -->
+    <!-- </c:choose> -->
+  </nav>
+</template>
+
+<style scoped></style>
